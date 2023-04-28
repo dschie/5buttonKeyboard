efe0dbbde367270339df01c6fe698c573c07a26a
Manuel Olbrich <manuel.olbrich@igd.fraunhofer.de>
2023-04-28 16:06:26 +0200

    fixed directory structure for ide and added debouncing

--- arduino/5ButtonKeyboard/5ButtonKeyboard.ino
+++ arduino/5ButtonKeyboard/5ButtonKeyboard.ino
@@ -1,11 +1,12 @@
 #include <Keyboard.h>
 
 #define keyCount 5
+#define debounceDelay 20
 
 uint8_t keys[keyCount] = {'a', 'b', 'c', 'd', 'e'};
 int pins[keyCount] = {2,3,4,5,6};
 bool down[keyCount] = {false,false,false,false};
-
+unsigned long lastChange[keyCount] = {0,0,0,0,0};
 void setup() {
 
 for(int i = 0; i < keyCount; i++)
@@ -15,14 +16,20 @@ for(int i = 0; i < keyCount; i++)
 void loop() {
   for(int i = 0; i < keyCount; i++)
   {
+    // just ignore this button as long as the delay hasn't passed
+    if ((lastChange[i]+debounceDelay) > millis())
+      return; 
+
     if (!down[i] && digitalRead(pins[i]) == LOW)
     {
       Keyboard.press(keys[i]);
+      lastChange[i] = millis();
       down[i] = true;
     }
     if (down[i] && digitalRead(pins[i]) == HIGH)
     {
       Keyboard.release(keys[i]);
+      lastChange[i] = millis();
       down[i] = false;
     }
   }
